4cf40556319df8343899a109d0d4d954c7e32354
Petr_yg00@itstepru.onmicrosoft.com <Petr_yg00@itstepru.onmicrosoft.com>
2022-06-23 15:05:32 +0300

    2DimenArray

--- Homework2.0/Source.cpp
+++ Homework2.0/Source.cpp
@@ -18,6 +18,7 @@ void Print(double arr[ROWS][COLS], const int ROWS, const int COLS);
 void Print(char arr[ROWS][COLS], const int ROWS, const int COLS);
 void Print(float arr[ROWS][COLS], const int ROWS, const int COLS);
 
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -53,6 +54,7 @@ void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
 	}
 	cout << endl;
 }
+
 void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
 {
 	for (int i = 0; i < ROWS; i++)
